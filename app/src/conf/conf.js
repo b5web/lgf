f71923fb186781c0770d1eace0e49004f667d4c9
b5web <cedric.casimir@yahoo.fr>
2017-05-02 20:36:19 +0200

    add config json 2

---
+++ app/src/conf/conf.js
@@ -0,0 +1,116 @@
+var configuration = {
+    main :
+      {
+        date : "2017-06-17 11:00",
+        title : "17 Juin 2017",
+        resume : "10eme Edition", 
+        image : "",
+        subscription :
+            {
+               display : true,
+               description :
+                    "Inscrivez vous pour l'édition 2017 qui aura lieu le 17 Juin 2017 à la salle polyvalente de Plouvorn(29).<br/>" +
+                    " Le coût de l'inscription est de 20 euros par équipe. Des surprises sont prévues pour fêter les 10 ans du tournoi.<br/>" +
+                    " Attention, une personne au moins dans l'équipe doit être majeure"
+            },
+        countDown : true
+      },
+    editions : [
+        {
+          name: "Edition 2016",
+          description: 
+              "Finale:<br/> " +
+              "Finale consolante: Groupirs<br/> " +
+              "Meilleur déguisement: Groupirs<br/>" +
+              " <a href = \"https://www.facebook.com/pg/La-Grande-Flaque-148040192047018/photos/?tab=album&album_id=536201693230864\" target = \"_blank\">" +
+              " Retrouvez toute les photos en cliquant ici </a>",
+          image: "edition-2016.jpg"
+        } ,
+        {
+          name: "Edition 2015",
+          description: 
+                "Les finalistes 2015 ! Clodo et ses clodettes et les pirates de bel air!<br>" +
+                "Palmarès meilleur déguisement : les Bée Do Bée do",
+          image: "edition-2015.jpg"
+        },
+        {
+          name: "Edition 2014",
+          description: 
+              "Bravo aux formules farm pour leur belle victoire!!",
+          image: "edition-2014.jpg"
+        },
+        {
+          name: "Edition 2013",
+          description:
+              "1) Sapeur Pompeur<br>" +
+              "2) Spounter Brini<br>" +
+              "3) Les goemoniers<br>" +
+              "4) La tribu africaine<br>" +
+              "5) Pacman<br>" +
+              "6) Les ringards<br>" +
+              "7) Les hand ouilles<br>" +
+              "8 Les filles des légendes<br>" +
+              "9) Les pires contre attaque<br>" +
+              "10) Plouvrogne<br>" +
+              "11) Pimor<br>" +
+              "12) On s'la colle<br>" +
+              "13) On se l'écaille<br>" +
+              "14) La croisière ça use<br>" +
+              "15) Bara Beach<br>" +
+              "16) Disco Beach",
+          image: "edition-2013.jpg"
+        },
+        {
+          name: "Edition 2012",
+          description:
+              "Palmarès:<br>" +
+              "- Vainqueurs: les irréductibles<br>" +
+              "- Vainqueurs consolante: les Tin Team<br>" +
+              "- Le fairplay: Les vaches à liche<br>" +
+              "- Le meilleur déguisement: les gros mickey",
+          image: "edition-2012.jpg"
+        },
+        {
+          name: "Edition 2011",
+          description:
+              "Palmarès:<br>" +
+              "- Gagnant: Les pieds qui puent<br>" +
+              "- Finaliste: Les loisirs<br>" +
+              " - 1er consolante: Les forcenés du tonneau<br>" +
+              "- 2ème consolante: Blanche 16 et les 7 pleins<br>" +
+              "- Meilleur déguisement: Blanche 16 et les 7 pleins<br>" +
+              "- Meilleur fair-play: Alice au pays du hand",
+          image: "edition-2011.jpg"
+        }   ,
+        {
+          name: "Edition 2010",
+          description:
+              ">Le succès ne se dément pas d'une année sur l'autre. " +
+              "La Grande Flaque a réuni une quinzaine d'équipes pour fêter le hand dans la joie et la bonne humeur. " +
+              "Les Crazy Guns ont dégainé les premiers et ont remporté la finale face aux Blues Brothers. " +
+              "L'équipe de Fanch grâce à son vibrant hommage à Raymond et à son fair play ont remporté la coupe du " +
+              "meilleur déguisement et du fair play.",
+          image: "edition-2010.jpg"
+        },
+        {
+          name: "Edition 2009",
+          description: 
+              "La 2nde édition de la Grande Flaque s'est déroulée le samedi 6 juin 2009." +
+              " Après un match disputé, les Vieilles Gloires de Plouvorn ont gagné la finale face au DiscoBitch" +
+              " qui était pourtant au top de la Disco attitude. L équipe des Rabbi Jacob ont gagné pour " +
+              "la deuxième année consécutive le trophée du meilleur déguisement grâce à une chorégraphie " +
+              "qui a enchanté le public présent.",
+          image: "edition-2009.jpg"
+        } ,
+        {
+          name: "Edition 2008",
+          description:
+              "La 1ere édition du tournoi de handball « la Grande Flaque » s’est déroulée " +
+              "le samedi 6 juin 2008. 18 équipes venues des environs se sont rencontrées dans une ambiance conviviale " +
+              "et colorée grâce aux déguisements et à la bonne humeur des joueurs. Pour cette première, " +
+              "c’est l’équipe des Manchaks (Morlaix) qui a remporté le tournoi après avoir battu 13 - 10 " +
+              "l’équipe Loisir de Plouvorn.",
+          image: "edition-2008.jpg"
+        }
+    ]
+}
